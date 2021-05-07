55a24adf6b095003d3aa7975b95905fb3a3f0f56
gracedebenedett <debenedettig@carleton.edu>
2021-05-07 13:56:01 -0500

    some work not for submission

--- authentication/authentication.py
+++ authentication/authentication.py
@@ -1,34 +1,77 @@
+#First attempt, not used in final
 import hashlib
 import binascii
 
-def decryptMD5():
-    passwords = [line.strip().lower() for line in open('passwords1.txt')]
+def createMD5Dict():
+    MD5Dict = {}
+    passwords = [line.strip().lower() for line in open('givenpasswords1.txt')]
+    for login in passwords:
+        login = login.split(":")
+        username = login[0]
+        MDhash = login[1]
+        MD5Dict[MDhash] = username
+    return MD5Dict
+
+def createWordDict():
+    hashCount = 0
+    wordDict = {}
     words = [line.strip().lower() for line in open('words.txt')]
-    foundPasswords = {}
+    for password in words:
+        encodedPassword = password.encode('utf-8')
+        md5 = hashlib.md5(encodedPassword)
+        passwordHash = md5.digest()
+        passwordHashAsHex = binascii.hexlify(passwordHash)
+        passwordHashAsHexString = passwordHashAsHex.decode('utf-8')
+        hashCount += 1
+        wordDict[passwordHashAsHexString] = password
+    print(hashCount)
+    return wordDict
+
+def createSaltHash():
+    saltMD5Dict = {}
+    passwords = [line.strip().lower() for line in open('givenpasswords2.txt')]
     for login in passwords:
         login.split(":")
         username = login[0]
-        MDhash = login[1]
-        for password in words:
-            # Compute the MD5 hash of this example password
-            #print('password ({0}): {1}'.format(type(password), password))
+        MDhash = login[1] #this includes salt$salthash+passwordhash
+        MDhash = MDhash.split("$")
+        salt = MDhash[0]
+        encodedSalt = salt.encode('utf-8')
+        md5Salt = hashlib.md5(encodedSalt)
+        saltHash = md5Salt.digest()
+        saltHashAsHex = binascii.hexlify(saltHash)
+        saltHashAsHexString = saltHashAsHex.decode('utf-8')
+        hashSalt = MDhash[1] #salt hash concatenated with password hash
+        saltMD5Dict[hashSalt] = username
+    return saltHashAsHexString, saltMD5Dict
 
-            encodedPassword = password.encode('utf-8') # type=bytes
-            #print('encodedPassword ({0}): {1}'.format(type(encodedPassword), encodedPassword))
+createSaltedWordDict():
+    hashCount = 0
+    wordDict = {}
+    words = [line.strip().lower() for line in open('words.txt')]
+    for password in words:
+        encodedPassword = password.encode('utf-8')
+        md5 = hashlib.md5(encodedPassword)
+        passwordHash = md5.digest()
+        passwordHashAsHex = binascii.hexlify(passwordHash)
+        passwordHashAsHexString = passwordHashAsHex.decode('utf-8')
+        hashCount += 1
+        wordDict[passwordHashAsHexString] = password
+    print(hashCount)
+    return wordDict
 
-            md5 = hashlib.md5(encodedPassword)
-            passwordHash = md5.digest() # type=bytes
-            #print('passwordHash ({0}): {1}'.format(type(passwordHash), passwordHash))
+def main():
+    numPasswords = 0
+    foundPasswords = {}
+    MD5Dict = createMD5Dict()
+    wordDict = createWordDict()
+    for key in MD5Dict:
+        if key in wordDict:
+            foundPasswords[MD5Dict[key]] = wordDict[key]
+            numPasswords += 1
+            #print (MD5Dict[key] + ":" + wordDict[key])
+    print(numPasswords)
 
-            passwordHashAsHex = binascii.hexlify(passwordHash) # weirdly, still type=bytes
-            #print('passwordHashAsHex ({0}): {1}'.format(type(passwordHashAsHex), passwordHashAsHex))
-
-            passwordHashAsHexString = passwordHashAsHex.decode('utf-8') # type=string
-            #print('passwordHashAsHexString ({0}): {1}'.format(type(passwordHashAsHexString), passwordHashAsHexString))
-            if passwordHashAsHexString == MDhash:
-                foundPasswords[username] = password
-                print(foundPasswords)
-    return foundPasswords
 
 if __name__ == "__main__":
-    decryptMD5()
+    main()
